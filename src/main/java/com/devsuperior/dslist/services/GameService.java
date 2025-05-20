58dcb74ff80bc18de583567260e4fda64a1419e7
Tony Sampaio <tonysampa@gmail.com>
2025-05-20 16:17:24 -0400

    N-N Classe de associação, embedded id, JPA, Projections

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -29,8 +31,15 @@ public class GameService {
 		return new GameDTO(result);		
 	}
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result =  gameRepository.findAll();
+		return result.stream().map(g -> new GameMinDTO(g)).toList();
+	}
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(@PathVariable Long listId) {
+		List<GameMinProjection> result =  gameRepository.searchByList(listId);
 		return result.stream().map(g -> new GameMinDTO(g)).toList();		
 	}
 	
