d4c51ac79daa1cf8cc2f71a67ef29458f4da7e9f
Tony Sampaio <tonysampa@gmail.com>
2025-05-21 18:05:30 -0400

    Homolog ok

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -12,13 +12,18 @@ import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(value = "/games")
+@Tag(name = "End point de Jogos")
 public class GameController {
 	
 	@Autowired
 	private GameService gameService;
 	
+	@Operation(summary = "Encontre jogo por Id", description = "Este método use para entrar com id, que trará as info completa do jogo")
 	@GetMapping(value = "/{id}")
 	public GameDTO findById(@PathVariable Long id) {
 		GameDTO result = gameService.findById(id);
